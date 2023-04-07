606ce0b61bb3a33eb71a0cf4a0b117b957ca4bc6
JaredLarios <lar21038@byui.edu>
2023-04-07 15:41:43 -0600

    deploy

--- dope/dope/settings.py
+++ dope/dope/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(5updx%48g1hm0n1es0^35em6hj5^f_3ud&2wd%*krcjqy4-iv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
